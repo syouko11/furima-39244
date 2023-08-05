04594ebaa19892187427f9a3c6c34725b9378e5a
syouko11 <piano19980226Nanami@icloud.com>
2023-08-05 18:41:39 +0900

    deviseにストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
-
+  #deviseに関するコントローラーの処理であれば、その時だけconfigure_permitted_parametersメソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  #新規登録の処理に対して、email,encrypted_password以外のキーのパラメーターも許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
