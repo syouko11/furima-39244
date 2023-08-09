7a8c624254e36808f8562ff5108ae7ff14464c5d
syouko11 <piano19980226Nanami@icloud.com>
2023-08-09 16:25:51 +0900

    パスワードの英数字混合のメッセージ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     # パスワードが必須、6文字以上必須、パスワードとパスワード（確認）の一致は必須であることはデフォルトで備わっているため記述不要。
     # パスワードが半角英数字混合での入力必須であること。
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: ''
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
     # お名前(全角)は、名字と名前がそれぞれ必須であること。全角の漢字・ひらがな・カタカナ
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
