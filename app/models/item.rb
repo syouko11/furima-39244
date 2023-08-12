9c49de1554576e09a31af43c47fc862026b0373a
syouko11 <piano19980226Nanami@icloud.com>
2023-08-12 14:50:31 +0900

    categoryモデル作成とアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user
 
   # ActiveStorageのアソシエーション
   has_one_attached :image
+
+  # ActiveHashのアソシエーション
+  belongs_to :category
 
 end
