11c93da57ce8f0a3e4ec389e966c2f5a3714cf3e
syouko11 <piano19980226Nanami@icloud.com>
2023-09-03 20:35:55 +0900

    Formオブジェクトのインスタンス生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
+  # ログアウト状態で商品購入ページに遷移しようとするユーザーをログインページに遷移
+  before_action :authenticate_user!
 
   def index
-    
+    @order_form = OrderForm.new
   end
 
   def create
