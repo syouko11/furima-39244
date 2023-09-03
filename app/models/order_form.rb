797abb6c7423f613544aa3164f9bd9fa332b3a53
syouko11 <piano19980226Nanami@icloud.com>
2023-09-03 17:43:22 +0900

    データをテーブルに保存する処理

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -19,4 +19,10 @@ class OrderForm
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/}
   end
 
+  def save
+    # 購入情報を保存し、変数orderに代入
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 発送先情報を保存(order_idは、変数orderのidと指定)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
