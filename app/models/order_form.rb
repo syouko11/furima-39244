65403e0362b66d413cd953f93884ae07c048f521
syouko11 <piano19980226Nanami@icloud.com>
2023-09-14 18:58:18 +0900

    orderモデルのバリデーションを追加

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,6 +4,10 @@ class OrderForm
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    # orderモデルののバリデーション
+    validates :user_id
+    validates :item_id
+    
     # destinationモデルのバリデーション
     # 郵便番号は、「3桁ハイフン4桁」の半角文字列のみ保存可能
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
